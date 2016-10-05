cc57f318deada980630d7f9e6a2f70175a59e762
leduong <24duong@gmail.com>
2016-10-05 20:38:59 +0700

    tai lai

---
+++ app/src/main/java/com/ken/hauiclass/activity/ChatActivity.java
@@ -0,0 +1,28 @@
+package com.ken.hauiclass.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import com.ken.hauiclass.R;
+
+/**
+ * Created by Faker on 8/18/2016.
+ */
+public class ChatActivity extends Activity {
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.arg1==0){
+// 12 123123123123
+//le hong duong
+            }
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reset_pass);
+    }
+}
